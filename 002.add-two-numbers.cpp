d9ab3e59858a3c6994ecb22621239c463b4fe7d0
Junwu Zhang <junwuzhang@DNa1c44ab.SUNet>
2019-02-20 01:28:19 -0800

    graph and math

--- 002.add-two-numbers.cpp
+++ 002.add-two-numbers.cpp
@@ -1,3 +1,32 @@
+/*
+ * @lc app=leetcode id=2 lang=cpp
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (30.50%)
+ * Total Accepted:    757.3K
+ * Total Submissions: 2.5M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ */
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -37,6 +66,7 @@ public:
             ListNode* lastNode = new ListNode(carryOver);
             prev->next = lastNode;
         }
-        return start;       
+        return start;
+        
     }
 };
---
+++ 94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=94 lang=cpp
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (54.81%)
+ * Total Accepted:    404.3K
+ * Total Submissions: 737.8K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> output;
+        inorder(root, output);
+        return output;
+    }
+    
+    void inorder(TreeNode* root, vector<int>& output) {
+        if (!root) {
+            return;
+        }
+        inorder(root->left, output);
+        output.push_back(root->val);
+        inorder(root->right, output);
+    }  
+};
