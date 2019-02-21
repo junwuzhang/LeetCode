d3c11bbb7bd9c25b2e642d9c2cb69a2dfab97e72
Junwu Zhang <junwuzhang@DNa1c44ab.SUNet>
2019-02-21 00:06:57 -0800

    linked list

---
+++ 328.odd-even-linked-list.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ *
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (47.97%)
+ * Total Accepted:    132.3K
+ * Total Submissions: 275.7K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a singly linked list, group all odd nodes together followed by the
+ * even nodes. Please note here we are talking about the node number and not
+ * the value in the nodes.
+ * 
+ * You should try to do it in place. The program should run in O(1) space
+ * complexity and O(nodes) time complexity.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 1->3->5->2->4->NULL
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2->1->3->5->6->4->7->NULL
+ * Output: 2->3->6->7->1->5->4->NULL
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The relative order inside both the even and odd groups should remain as it
+ * was in the input.
+ * The first node is considered odd, the second node even and so on ...
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        ListNode *pt1;
+        ListNode *pt2;
+        pt1 = head;
+        pt2 = head->next;
+        pt3 = head->next;
+        while(pt1->next->next || pt2->next->next){
+            pt1->next = pt2->next;
+            pt1 = pt1->next;
+            pt2->next = pt1->next;
+            pt2 = pt2->next;
+        }
+        pt1->next = pt3;
+        return head;
+    }
+};
